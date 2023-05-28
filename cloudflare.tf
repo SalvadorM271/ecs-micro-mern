c755556a56401333c53ef0e26151b093444d9f6a
salvadorM271 <guardianes27@outlook.com>
2023-05-28 00:31:41 -0600

    added secrets manager

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,6 +1,14 @@
+data "aws_secretsmanager_secret_version" "cloudflare_secret" {
+  secret_id = "arn:aws:secretsmanager:us-east-1:153042419275:secret:cloudflare-ZlOXvE"
+}
+
 provider "cloudflare" {
-  email = "" // cloudflare email here
-  api_key = "" // api key here
+  email = jsondecode(data.aws_secretsmanager_secret_version.cloudflare_secret.secret_string)["cloudflare_email"] // cloudflare email here
+  api_key = jsondecode(data.aws_secretsmanager_secret_version.cloudflare_secret.secret_string)["cloudflare_key"] // api key here
+}
+
+output "username" {
+  value = jsondecode(data.aws_secretsmanager_secret_version.cloudflare_secret.secret_string)["cloudflare_email"]
 }
 
 resource "cloudflare_record" "record" {
--- dev.tfvars
+++ dev.tfvars
@@ -1,4 +1,4 @@
-region = "us-east-2"
+region = "us-east-1"
 environment = "dev"
 name = "ecs-microservice"
 
